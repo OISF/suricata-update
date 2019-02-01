1b59bc7160a94fb677419c824c6b31f436203b26
Jason Ish <ish@unx.ca>
2019-02-01 12:15:06 -0600

    Optimize report generation memory usage
    Avoid the need to keep the complete parsed old ruleset in memory
    while building the report by generating the report as the old
    ruleset is parsed.
    
    In my tests has a good amount of memory saving.
    
    Redmine issue:
    https://redmine.openinfosecfoundation.org/issues/2791

--- suricata/update/main.py
+++ suricata/update/main.py
@@ -566,75 +566,76 @@ def load_dist_rules(files):
                 logger.error("Failed to open %s: %s" % (path, err))
                 sys.exit(1)
 
-def build_report(prev_rulemap, rulemap):
-    """Build a report of changes between 2 rulemaps.
-
-    Returns a dict with the following keys that each contain a list of
-    rules.
-    - added
-    - removed
-    - modified
-    """
-    report = {
-        "added": [],
-        "removed": [],
-        "modified": []
-    }
-
-    for key in rulemap:
-        rule = rulemap[key]
-        if not rule.id in prev_rulemap:
-            report["added"].append(rule)
-        elif rule.format() != prev_rulemap[rule.id].format():
-            report["modified"].append(rule)
-    for key in prev_rulemap:
-        rule = prev_rulemap[key]
-        if not rule.id in rulemap:
-            report["removed"].append(rule)
-
-    return report
-
 def write_merged(filename, rulemap):
 
     if not args.quiet:
-        prev_rulemap = {}
+        # List of rule IDs that have been added.
+        added = []
+        # List of rule objects that have been removed.
+        removed = []
+        # List of rule IDs that have been modified.
+        modified = []
+
+        oldset = {}
         if os.path.exists(filename):
-            prev_rulemap = build_rule_map(
-                suricata.update.rule.parse_file(filename))
-        report = build_report(prev_rulemap, rulemap)
+            for rule in suricata.update.rule.parse_file(filename):
+                oldset[rule.id] = True
+                if not rule.id in rulemap:
+                    removed.append(rule)
+                elif rule.format() != rulemap[rule.id].format():
+                    modified.append(rulemap[rule.id])
+        for key in rulemap:
+            if not key in oldset:
+                added.append(key)
+
         enabled = len([rule for rule in rulemap.values() if rule.enabled])
         logger.info("Writing rules to %s: total: %d; enabled: %d; "
                     "added: %d; removed %d; modified: %d" % (
                         filename,
                         len(rulemap),
                         enabled,
-                        len(report["added"]),
-                        len(report["removed"]),
-                        len(report["modified"])))
+                        len(added),
+                        len(removed),
+                        len(modified)))
     
     with io.open(filename, encoding="utf-8", mode="w") as fileobj:
         for rule in rulemap:
             print(rulemap[rule].format(), file=fileobj)
 
 def write_to_directory(directory, files, rulemap):
+    # List of rule IDs that have been added.
+    added = []
+    # List of rule objects that have been removed.
+    removed = []
+    # List of rule IDs that have been modified.
+    modified = []
+
+    oldset = {}
     if not args.quiet:
-        previous_rulemap = {}
         for filename in files:
             outpath = os.path.join(
                 directory, os.path.basename(filename))
+
             if os.path.exists(outpath):
-                previous_rulemap.update(build_rule_map(
-                    suricata.update.rule.parse_file(outpath)))
-        report = build_report(previous_rulemap, rulemap)
+                for rule in suricata.update.rule.parse_file(outpath):
+                    oldset[rule.id] = True
+                    if not rule.id in rulemap:
+                        removed.append(rule)
+                    elif rule.format() != rulemap[rule.id].format():
+                        modified.append(rule.id)
+        for key in rulemap:
+            if not key in oldset:
+                added.append(rule.id)
+
         enabled = len([rule for rule in rulemap.values() if rule.enabled])
         logger.info("Writing rule files to directory %s: total: %d; "
                     "enabled: %d; added: %d; removed %d; modified: %d" % (
                         directory,
                         len(rulemap),
                         enabled,
-                        len(report["added"]),
-                        len(report["removed"]),
-                        len(report["modified"])))
+                        len(added),
+                        len(removed),
+                        len(modified)))
 
     for filename in sorted(files):
         outpath = os.path.join(
