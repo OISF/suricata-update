fc8813c3e2edf540171b287c98c2f54170cfaf99
Jason Ish <ish@unx.ca>
2019-04-23 23:03:07 -0600

    index update: use oisf index, not jasonish

--- suricata/update/data/update.py
+++ suricata/update/data/update.py
@@ -27,7 +27,7 @@ def embed_index():
     """Embed a copy of the index as a Python source file. We can't use a
     datafile yet as there is no easy way to do with distutils."""
     dist_filename = os.path.join(os.path.dirname(__file__), "index.py")
-    url = "https://raw.githubusercontent.com/jasonish/suricata-intel-index/master/index.yaml"
+    url = "https://raw.githubusercontent.com/oisf/suricata-intel-index/master/index.yaml"
     response = urlopen(url)
     index = yaml.safe_load(response.read())
     with open(dist_filename, "w") as fileobj:
