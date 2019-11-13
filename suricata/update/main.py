2762bce744c9ed3c1317f468607a6c69394bba58
Jason Ish <jason.ish@oisf.net>
2019-11-13 10:46:14 -0600

    cleanup: remove dead code branch in downloader

--- suricata/update/main.py
+++ suricata/update/main.py
@@ -199,7 +199,7 @@ class Fetch:
         logger.info("Done.")
         return self.extract_files(tmp_filename)
 
-    def run(self, url=None, files=None):
+    def run(self, url, files=None):
         if files is None:
             files = {}
         if url:
@@ -209,9 +209,6 @@ class Fetch:
             except URLError as err:
                 url = url[0] if isinstance(url, tuple) else url
                 logger.error("Failed to fetch %s: %s", url, err)
-        else:
-            for url in self.args.url:
-                files.update(self.fetch(url))
         return files
 
     def extract_files(self, filename):
